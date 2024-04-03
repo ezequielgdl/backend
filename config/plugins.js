6880cb800b4ba59cbc9ed833a317cbea7ac6a398
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-04-03 19:03:40 +0200

    changes

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => {
       config: {
         provider: "strapi-provider-firebase-storage",
         providerOptions: {
-          serviceAccount: require("../mnami-e5c83-firebase-adminsdk-iv8ws-0c07e6e5c1.json"),
+          serviceAccount: require(env("FIREBASE_ADMIN_KEY")),
           bucket: env("STORAGE_BUCKET_URL"),
           sortInStorage: true, // true | false
           debug: false, // true | false
@@ -13,3 +13,5 @@ module.exports = ({ env }) => {
     },
   };
 };
+
+// require("../mnami-e5c83-firebase-adminsdk-iv8ws-0c07e6e5c1.json")
